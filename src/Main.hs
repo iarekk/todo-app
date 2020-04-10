fe65246cacf3983102819c5ec6dac00683526aa3
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2020-04-11 00:13:19 +0100

    Add data path parser

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,20 @@ module Main(main) where
 
 import Options.Applicative
 
+defaultDataPath :: FilePath
+defaultDataPath = "~/.to-do.yaml"
+
+dataPathParser :: Parser FilePath
+dataPathParser = strOption $
+  value defaultDataPath 
+  <> long "data-path"
+  <> short 'p'
+  <> metavar "DATAPATH"
+  <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+
+
+--  argument auto ()
+
 type ItemIndex = Int
 
 itemIndexParser :: Parser ItemIndex
@@ -9,5 +23,7 @@ itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
 
 main :: IO ()
 main = do
-  itemIndex <- execParser (info (itemIndexParser) (progDesc "To-do list manager"))
-  putStrLn $ "itemIndex=" ++ show itemIndex
+  dataPath <- execParser (info (dataPathParser) (progDesc "To-do list manager"))
+  putStrLn $ "dataPath=" ++ show dataPath
+  -- itemIndex <- execParser (info (itemIndexParser) (progDesc "To-do list manager"))
+  -- putStrLn $ "itemIndex=" ++ show itemIndex
