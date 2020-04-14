a9222465254feed02ae70c4de9f42ef63270c0af
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2020-04-15 00:57:38 +0100

    Implement view command

--- src/Main.hs
+++ src/Main.hs
@@ -9,10 +9,12 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import           Data.Either.Combinators (rightToMaybe)
 import           Data.String.Utils(replace)
+import           Data.List.Safe((!!))
 import           Data.Time
 import qualified Data.Yaml as Yaml
 import           GHC.Generics
 import           Options.Applicative hiding (infoParser)
+import           Prelude hiding ((!!))
 import           System.Directory(getHomeDirectory)
 import           System.IO.Error
 
@@ -177,16 +179,7 @@ main = do
     homeDir <- getHomeDirectory
 
     let expandedDataPath = replace "~" homeDir dataPath
-
-    let dueBy = LocalTime (ModifiedJulianDay 0) (TimeOfDay 13 0 0)
-
-    writeToDoList expandedDataPath $ ToDoList
-        [ Item "title1" (Just "description1") (Just High) (Just dueBy)
-        , Item "title2" (Just "description2") (Just Low) (Just dueBy)
-        ]
-
-    toDoList <- readToDoList expandedDataPath
-    print toDoList
+    run expandedDataPath command
 
 readToDoList :: FilePath -> IO ToDoList
 readToDoList path = do
@@ -203,9 +196,28 @@ run dataPath Info = putStrLn "info"
 run dataPath Init = putStrLn "init"
 run dataPath List = putStrLn "list"
 run dataPath (Add item) = putStrLn $ "add: " ++ show item
-run dataPath (View itemIndex) = putStrLn $ "view #" ++ show itemIndex
+run dataPath (View itemIndex) = viewItem dataPath itemIndex
 run dataPath (Update itemIndex itemUpdate) = putStrLn $ "update item #" ++ show itemIndex ++ " with update " ++ show itemUpdate
 run dataPath (Remove itemIndex) = putStrLn $ "remove #" ++ show itemIndex
 
 writeToDoList :: FilePath -> ToDoList -> IO()
 writeToDoList path todos = BS.writeFile path (Yaml.encode todos)
+
+viewItem :: FilePath -> ItemIndex -> IO ()
+viewItem dataPath itemIndex = do
+    ToDoList items <- readToDoList dataPath
+    let mbItem = items !! itemIndex :: Maybe Item
+    case mbItem of
+        Nothing ->   putStrLn "Invalid item index"
+        Just item -> showItem itemIndex item
+
+showItem :: ItemIndex -> Item -> IO()
+showItem itemIndex (Item title mbDescription mbPriority mbDueBy) = do
+    putStrLn $ "#" ++ show itemIndex ++ ": " ++ title
+    putStrLn $ showField "Description" show mbDescription
+    putStrLn $ showField "Priority" show mbPriority
+    putStrLn $ showField "DueBy" (formatTime defaultTimeLocale "%Y/%m/%d %H:%M:%S") mbDueBy
+
+showField :: String -> (a -> String) -> Maybe a -> String
+showField fieldName f (Just x) = " * " ++ fieldName ++ ": " ++ f x
+showField fieldName _ Nothing = " * " ++ fieldName ++ ": " ++ "(not set)"
