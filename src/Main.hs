ab9f50c66d2bd236e381e9b152ce21753ad3bbd5
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2020-04-15 23:54:00 +0100

    Implement removal

--- src/Main.hs
+++ src/Main.hs
@@ -28,7 +28,7 @@ run dataPath List = putStrLn "list"
 run dataPath (Add item) = addItem dataPath item
 run dataPath (View itemIndex) = viewItem dataPath itemIndex
 run dataPath (Update itemIndex itemUpdate) = putStrLn $ "update item #" ++ show itemIndex ++ " with update " ++ show itemUpdate
-run dataPath (Remove itemIndex) = putStrLn $ "remove #" ++ show itemIndex
+run dataPath (Remove itemIndex) = removeItem dataPath itemIndex
 
 addItem :: FilePath -> Item -> IO()
 addItem path item = do
@@ -55,3 +55,21 @@ showField :: String -> (a -> String) -> Maybe a -> String
 showField fieldName f (Just x) = " * " ++ fieldName ++ ": " ++ f x
 showField fieldName _ Nothing = " * " ++ fieldName ++ ": " ++ "(not set)"
 
+removeItem :: FilePath -> ItemIndex -> IO()
+removeItem path itemIndex = do
+    ToDoList items <- readToDoList path
+    let mbItems = items `removeAt` itemIndex
+    case mbItems of
+        Nothing -> putStrLn "Invalid item index"
+        Just items' -> do
+            let toDoList = ToDoList items'
+            writeToDoList path toDoList
+
+removeAt :: [a] -> Int -> Maybe [a]
+removeAt xs idx = 
+    if idx < 0 || idx >= length xs
+    then Nothing
+    else
+        let (before, _:after) = splitAt idx xs
+            xs' = before ++ after
+        in Just xs'
