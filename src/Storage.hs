43116e63fabe5d66ceb972fa2611ec631340a8ef
Iarek Kovtunenko <16271112+iarekk@users.noreply.github.com>
2020-04-16 23:49:59 +0100

    Implement the rest of the commands

--- src/Storage.hs
+++ src/Storage.hs
@@ -1,24 +1,25 @@
-module Storage (readToDoList, writeToDoList) where
+module Storage (readToDoList, readToDoList', writeToDoList) where
 
 import           Control.Exception
 import qualified Data.ByteString.Char8 as BS
-import           Data.Either.Combinators (rightToMaybe)
 import qualified Data.Yaml as Yaml
 import           System.IO.Error
 
-
-
 import Types
+
+readToDoList' :: FilePath -> IO (Either Yaml.ParseException ToDoList)
+readToDoList' path =
+    Yaml.decodeEither' <$> BS.readFile path
 
 readToDoList :: FilePath -> IO ToDoList
 readToDoList path = do
     mbToDoList <- catchJust
         (\e -> if isDoesNotExistError e then Just () else Nothing)
-        (rightToMaybe . Yaml.decodeEither' <$> BS.readFile path)
-        (\_ -> return $ Just (ToDoList []))
+        (Yaml.decodeEither' <$> BS.readFile path)
+        (\_ -> return $ Right (ToDoList []))
     case mbToDoList of
-        Nothing -> error "YAML file is corrupt"
-        Just toDoList -> return toDoList
+        Left _ -> error "YAML file is corrupt"
+        Right toDoList -> return toDoList
 
 writeToDoList :: FilePath -> ToDoList -> IO()
 writeToDoList path todos = BS.writeFile path (Yaml.encode todos)
